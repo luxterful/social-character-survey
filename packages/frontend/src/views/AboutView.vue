9122ca62e71256fa9f485429bdf9a7c5efcd2155
luxterful <derlukasbauer@gmail.com>
2023-07-03 22:44:47 +0200

    adds intro form

--- packages/frontend/src/views/AboutView.vue
+++ packages/frontend/src/views/AboutView.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const pronone = ref('he')
+</script>
+
 <template>
-  <h1>What is it about?</h1>
+  <h1>Wie heißt du?</h1>
+  <input class="border border-black" />
+  <h1>Anrede?</h1>
+  <div class="bg-slate-300 rounded-md w-auto inline-block">
+    <div class="flex gap-1">
+      <button
+        class="p-2 rounded-md"
+        :class="[pronone == 'he' ? 'bg-slate-500 text-white' : 'hover:bg-slate-400']"
+        @click="pronone = 'he'"
+      >
+        he / him
+      </button>
+      <button
+        class="p-2 rounded-md"
+        :class="[pronone == 'she' ? 'bg-slate-500 text-white' : 'hover:bg-slate-400']"
+        @click="pronone = 'she'"
+      >
+        she / her
+      </button>
+      <button
+        class="p-2 rounded-md"
+        :class="[pronone == 'they' ? 'bg-slate-500 text-white' : 'hover:bg-slate-400']"
+        @click="pronone = 'they'"
+      >
+        they / them
+      </button>
+    </div>
+  </div>
 </template>
